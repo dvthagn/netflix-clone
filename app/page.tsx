a83f7837726085fb5f8fa56040188d564ff6b885
Thang dinh <thangdinh14102002@gmail.com>
2023-07-19 21:59:11 +0700

    first setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>netflix clone</h1>
+    </>
+  );
+}
---
+++ components/Input.tsx
@@ -0,0 +1,17 @@
+const Input = () => {
+  return (
+    <input
+      className="
+        block
+        rouded-md
+        px-6
+        pt-6
+        pb-1
+        w-full
+        text-md
+    "
+    />
+  );
+};
+
+export default Input;
