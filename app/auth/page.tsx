eb2477be7d213b010656219b6a30a2d269018038
Thang dinh <thangdinh14102002@gmail.com>
2023-07-19 23:47:42 +0700

    add layout register and login

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,19 @@
+"use client";
 import Input from "@/components/Input";
+import { useState, useCallback } from "react";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -9,10 +22,58 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
             <div className="flex flex-col gap-4">
-              <Input />
+              {variant === "register" && (
+                <Input
+                  id="name"
+                  onChange={(e: any) => setName(e.target.value)}
+                  value={name}
+                  label="Username"
+                />
+              )}
+
+              <Input
+                id="email"
+                onChange={(e: any) => setEmail(e.target.value)}
+                value={email}
+                label="Email"
+                type="email"
+              />
+              <Input
+                id="password"
+                onChange={(e: any) => setPassword(e.target.value)}
+                value={password}
+                label="Password"
+                type="password"
+              />
             </div>
+            <button className="bg-red-600 py-3 text-white w-full rounded-md mt-10 hover:bg-red-700 transition">
+              {variant === "login" ? "Login" : "Register  "}
+            </button>
+            {variant === "login" ? (
+              <p className="text-neutral-500 mt-12">
+                First time using Netflix?
+                <span
+                  onClick={toggleVariant}
+                  className="text-white ml-1 hover:underline cursor-pointer"
+                >
+                  Create new account
+                </span>
+              </p>
+            ) : (
+              <p className="text-neutral-500 mt-12">
+                Already have an account?
+                <span
+                  onClick={toggleVariant}
+                  className="text-white ml-1 hover:underline cursor-pointer"
+                >
+                  Login
+                </span>
+              </p>
+            )}
           </div>
         </div>
       </div>
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,7 +1,21 @@
-const Input = () => {
+import React from "react";
+
+interface InputProps {
+  id: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type?: string;
+}
+const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   return (
-    <input
-      className="
+    <div className="relative">
+      <input
+        onChange={onChange}
+        type={type}
+        value={value}
+        id={id}
+        className="
         block
         rouded-md
         px-6
@@ -9,8 +23,41 @@ const Input = () => {
         pb-1
         w-full
         text-md
+        text-white
+        bg-neutral-700
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        peer
+
     "
-    />
+        placeholder=" "
+      />
+
+      <label
+        htmlFor={id}
+        className="
+      absolute
+      text-md
+      text-zinc-400
+      duration-150
+      tranform
+      -translate-y-3
+      scale-75
+      top-4
+      z-10
+      origin-[0]
+      left-6
+      peer-placeholder-shown:scale-100
+      peer-placeholder-shown:translate-y-0
+      peer-focus:scale-75
+      peer-focus:-translate-y-3
+      
+      "
+      >
+        {label}
+      </label>
+    </div>
   );
 };
 
